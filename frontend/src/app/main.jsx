07324755d7585fe3f22d9f5007fcd9d848a4af42
Aleeykhan <aleeykhan11@gmail.com>
2026-09-14 05:17:01 +0000

    chore: intergrates AppThemeProvider into main

--- frontend/src/app/main.jsx
+++ frontend/src/app/main.jsx
@@ -3,11 +3,13 @@ import { StrictMode } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 import { StoreProvider } from './providers'
-
+import { AppThemeProvider } from '@/features/theme'
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <StoreProvider>
-            <RouterProvider router={router} />
+            <AppThemeProvider>
+                <RouterProvider router={router} />
+            </AppThemeProvider>
         </StoreProvider>
     </StrictMode>
 );
